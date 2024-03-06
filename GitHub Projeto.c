b750a43e5788479c817c403ecee38fa94a91b771
Pedro Henrique Candido <pedrohecan@gmail.com>
2024-03-06 18:24:31 -0300

    Programa de tabuada
    Um programa de tabuada

--- GitHub Projeto.c
+++ GitHub Projeto.c
@@ -2,5 +2,22 @@
 #include<stdlib.h>
 
 int main(){
-    int i;
+    int i,j;
+    float tabuada;
+
+    for ( i = 1; i <=10; i++)
+    {
+        printf("Tabuada do %d\n", i);
+        for (j = 1 ; j <= 10; j++)
+        {
+            tabuada = i*j;
+
+
+            printf("%f\n", tabuada);
+
+        }
+        
+    }
+    
 }
+
